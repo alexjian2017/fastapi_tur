eea56baa2ede999676066f1c39c7e74a3697e5c5
alexjian <jhn840530@gmail.com>
2023-11-26 20:26:31 +0800

    ass alembic

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI
-from numpy import deprecate
-
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from .utils import hash
 from .router import post, user, auth, vote
 from .models import Base
-from .database import engine, get_db
+from .database import engine
 
-# uvicorn main:app --host 127.0.0.1 --port 8000 --reload
+# uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import uvicorn
 
 from app import main
 
+# Python API Development - Comprehensive Course for Beginners
+# https://www.youtube.com/watch?v=0sOvCWFmrtA
+
 if __name__ == "__main__":
-    uvicorn.run(main.app, port=8000, host="127.0.0.1")
+    uvicorn.run(main.app, port=8000, host="0.0.0.0")
